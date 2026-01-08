52c3663f81c4d20ac6110547981580b6179232ca
Shakti Priya <priyashakti68@gmail.com>
2026-01-08 15:45:50 +0530

    add salebanner image

---
+++ client/src/components/SaleBanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/SaleBanner.css";
+const SaleBanner = () => {
+  return (
+    <div className="full-width-banner-container">
+      <img
+        src="/sale.webp"
+        alt="Republic Day Sale Banner"
+        className="banner-image"
+      />
+    </div>
+  );
+};
+
+export default SaleBanner;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import ProductSection from "../components/ProductSection";
 import FeaturedGrid from "../components/FeaturedGrid";
 import RecentlyViewed from "../components/RecentlyViewed";
 import Footer from "../components/Footer";
+import SaleBanner from "../components/SaleBanner.jsx";
 
 const Home = () => {
   return (
@@ -12,12 +13,26 @@ const Home = () => {
       {/* <CategoryStrip /> */}
       <div className="home-content">
         <Carousel />
-        
+        <SaleBanner />
+
         {/* Dynamic Sections - Row 1 */}
-        <div style={{ display: 'flex', gap: '10px', margin: '10px', flexWrap: 'wrap' }}>
-            <FeaturedGrid title="Top Selection" sectionTag="top_deals" />
-            <FeaturedGrid title="Best Deals on Furniture" sectionTag="furniture_bestsellers" />
-            <FeaturedGrid title="Make your home stylish" sectionTag="home_stylish" />
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            margin: "10px",
+            flexWrap: "wrap",
+          }}
+        >
+          <FeaturedGrid title="Top Selection" sectionTag="top_deals" />
+          <FeaturedGrid
+            title="Best Deals on Furniture"
+            sectionTag="furniture_bestsellers"
+          />
+          <FeaturedGrid
+            title="Make your home stylish"
+            sectionTag="home_stylish"
+          />
         </div>
 
         {/* Existing Horizontal Section */}
@@ -27,10 +42,20 @@ const Home = () => {
         />
 
         {/* Dynamic Sections - Row 2 */}
-        <div style={{ display: 'flex', gap: '10px', margin: '10px', flexWrap: 'wrap' }}>
-             <FeaturedGrid title="Best Value Deals on Fashion" sectionTag="fashion_value" />
-             <FeaturedGrid title="Top Deals on Gadgets" sectionTag="gadgets" />
-             <FeaturedGrid title="Discounts for You" sectionTag="discounts" />
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            margin: "10px",
+            flexWrap: "wrap",
+          }}
+        >
+          <FeaturedGrid
+            title="Best Value Deals on Fashion"
+            sectionTag="fashion_value"
+          />
+          <FeaturedGrid title="Top Deals on Gadgets" sectionTag="gadgets" />
+          <FeaturedGrid title="Discounts for You" sectionTag="discounts" />
         </div>
 
         <ProductSection
